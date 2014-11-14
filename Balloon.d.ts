9051daa33350c228e5223297227d67d4b8b50f8a
Legokichi Duckscallion <legokichi@duxca.com>
2014-11-15 07:05:15 +0900

    add ballonsurface

--- Balloon.d.ts
+++ Balloon.d.ts
@@ -1,14 +1,28 @@
+interface JSZipDirectory { [filepath: string]: JSZipObject; };
+interface Descript { [key: string]: string; };
+interface Balloons {
+  sakura: {descript: Descript; canvas: HTMLCanvasElement; }[];
+  kero: {descript: Descript; canvas: HTMLCanvasElement; }[];
+  communicate: {descript: Descript; canvas: HTMLCanvasElement; }[];
+  online: {descript: Descript; canvas: HTMLCanvasElement; }[];
+  arrow: {descript: Descript; canvas: HTMLCanvasElement; }[];
+  sstp: {descript: Descript; canvas: HTMLCanvasElement; }[];
+  thumbnail: {descript: Descript; canvas: HTMLCanvasElement; }[];
+}
+
 
 declare class Balloon {
-  constructor(tree: any); // unstable
+  constructor(directory: JSZipDirectory); // stable
   load(callback:(error: any) => void): void; // stable
-  getSurface(scopeId: number, surfaceId: number): BalloonSurface; // stable
-  descript: { [key: string]: string; }; // stable
-  tree: any; // unstable
-  surfaces: any; // unstable
+  getSurface(scopeId: number, surfaceId: number): Blimp; // unstable
+  descript: Descript; // stable
+  directory: JSZipDirectory; // stable
+  balloons: Balloons; // stable
 }
 
 declare module Balloon {
+  loadBalloonDescripts(directory: JSZipDirectory, balloons: Balloons, descript: Descript): void
+  loadBalloonSurfaces(directory: JSZipDirectory, balloons: Balloons, callback: (error: any) => void): void
 }
 
 declare module 'balloon' {
--- BalloonSurface.d.ts
+++ BalloonSurface.d.ts
@@ -1,19 +1,15 @@
 
 declare class BalloonSurface {
-  constructor(scopeId: number, srf: any, surfaces: any); // unstable
+  constructor(scopeId: number, surfaceId: number, balloons: Balloons); // stable
   destructor(): void; // stable
   element: HTMLCanvasElement; // stable
   render(): void; // stable
-  playAnimation(animationId, callback: () => void): void; // stable
-  stopAnimation(): void; // stable
+  playAnimation(): void; // unstable
+  stopAnimation(): void; // unstable
 }
 
 
 declare module BalloonSurface {
-  function random(callback: (callback: () => void) => void, probability: Number): void; // stable
-  function periodic(callback: (callback: () => void) => void, sec: Number): void; // stable
-  function always(callback: (callback: () => void) => void): void; // stable
-  function isHit(cnv: HTMLCanvasElement, x: number, y: number ): boolean; // stable
 }
 
 declare module 'balloonsurface' {
